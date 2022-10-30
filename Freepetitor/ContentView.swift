e3add713f96468aa19ecafad9b66506f1e928fcd
Rashid Gaitov <rashidgaitov@gmail.com>
2022-10-30 13:59:12 +0300

    Initial Commit

---
+++ Freepetitor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Freepetitor
+//
+//  Created by Rashid Gaitov on 30.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Freepetitor/FreepetitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreepetitorApp.swift
+//  Freepetitor
+//
+//  Created by Rashid Gaitov on 30.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct FreepetitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
