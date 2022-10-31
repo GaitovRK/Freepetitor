ec8caa351c180afea39dad0b7091f3314aeb3132
Rashid Gaitov <rashidgaitov@gmail.com>
2022-10-31 10:43:19 +0300

    Added country Picker, Login screen

--- Freepetitor/ContentView.swift
+++ Freepetitor/ContentView.swift
@@ -8,9 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var nameTextFieldText: String = ""
+    @State var surnameTextFieldText: String = ""
+    @State var savedText: [String] = []
+    @State var selectedCountry = " "
+    @State var countries = [
+        "Kazakhstan",
+        "Poland",
+        "Turkey",
+        "Japan"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                VStack {
+                TextField("Name", text: $nameTextFieldText)
+                TextField("Surname", text: $surnameTextFieldText)
+                }
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button(action: {
+                    saveText(text: nameTextFieldText)
+                    saveText(text: surnameTextFieldText)
+                    nameTextFieldText = ""
+                    surnameTextFieldText = ""
+
+                }, label: {
+                    Text("LOGIN")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue.cornerRadius(15))
+                        .foregroundColor(Color.white)
+                        .font(.headline)
+                })
+                .padding()
+                
+                Text("Selected country: \(selectedCountry)")
+                Picker(selection: $selectedCountry) {
+                    Text("Choose country")
+                    ForEach(countries, id: \.self) { country in
+                        Text(country)
+                    }
+                } label: {
+                    
+                }
+                .pickerStyle(.inline)
+
+                
+                Spacer()
+            }
+            .navigationTitle("Login")
+
+        }
+
+    }
+    
+    func saveText(text: String) {
+        savedText.append(text)
     }
 }
 
