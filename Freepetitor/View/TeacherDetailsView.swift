9cb515d2899f9b71e8f089e9bb3b5241d115adcc
Rashid Gaitov <rashidgaitov@gmail.com>
2022-11-10 22:57:24 +0300

    Work on TeacherDetailsView

--- Freepetitor/View/TeacherDetailsView.swift
+++ Freepetitor/View/TeacherDetailsView.swift
@@ -9,12 +9,85 @@ import SwiftUI
 
 struct TeacherDetailsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Header()
+        
     }
 }
 
 struct TeacherDetailsView_Previews: PreviewProvider {
     static var previews: some View {
         TeacherDetailsView()
+    }
+}
+
+struct Header: View {
+    var body: some View {
+        VStack {
+            HStack(spacing: 40) {
+                Image("img1")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100)
+                    .cornerRadius(40)
+                
+            }
+            VStack {
+                Text("Rashid Gaitov")
+                    .font(.title2)
+                Text("iOS Developer")
+                    .font(.subheadline)
+            }
+            
+            List {
+
+                EducationView()
+                
+                ExperienceView()
+                
+                
+            }
+            Spacer()
+        }
+    }
+}
+
+
+struct EducationView: View {
+    var body: some View {
+        
+        Section {
+            VStack(alignment: .leading) {
+                Text("Bogazici University")
+                    .font(.headline)
+                Text("Master's Degree in Software Engineering")
+                    .font(.callout)
+            }
+            VStack(alignment: .leading) {
+                Text("Nazarbayev University")
+                    .font(.headline)
+                Text("Bachelor's Degree in Civil Engineering")
+                    .font(.callout)
+            }
+
+        } header: {
+            Text("Education")
+        }
+        
+    }
+}
+
+
+struct ExperienceView: View {
+    var body: some View {
+        
+        Section {
+            Text("Experience 1")
+            Text("Experience 2")
+
+        } header: {
+            Text("Experience")
+        }
+        
     }
 }
